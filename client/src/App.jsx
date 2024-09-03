6dc2fe0fc804b2a5e132bfe68e102079a2ae0545
dharaneesh1212 <dharun2122002@gmail.com>
2024-09-03 13:36:40 +0530

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Employee from "./components/employee";
+
 const App = () => {
   return (
     <main>
-
+      <Employee />
     </main>
   );
 };
---
+++ client/src/components/employee.jsx
@@ -0,0 +1,62 @@
+// import { FaUserEdit } from "react-icons/fa";
+// import { MdDelete } from "react-icons/md";
+
+const Employee = () => {
+  return (
+    <main className="h-screen w-screen bg-black flex items-start justify-center p-20">
+      <div className="flex items-center justify-center flex-col gap-10 capitalize">
+        <div className="flex items-center justify-center gap-8">
+          <input
+            className="h-8 w-32 rounded-md p-1 text-lg outline-none"
+            type="number"
+            placeholder="Employee Id"
+            name="employeeId"
+          />
+          <input
+            className="h-8 w-52 rounded-md p-1 text-lg outline-none"
+            type="text"
+            placeholder="First Name"
+            name="firstname"
+          />
+          <input
+            className="h-8 w-52 rounded-md p-1 text-lg outline-none"
+            type="text"
+            placeholder="Last Name"
+            name="lastname"
+          />
+          <input
+            className="h-8 w-52 rounded-md p-1 text-lg outline-none"
+            type="email"
+            placeholder="Email"
+            name="email"
+          />
+        </div>
+        <div className="flex items-center justify-center gap-8">
+          <input
+            className="h-8 w-52 rounded-md p-1 text-lg outline-none"
+            type="text"
+            placeholder="Position"
+            name="position"
+          />
+          <input
+            className="h-8 w-52 rounded-md p-1 text-lg outline-none"
+            type="text"
+            placeholder="Department"
+            name="department"
+          />
+          <input
+            className="h-8 w-40 rounded-md p-1 text-lg outline-none"
+            type="date"
+            placeholder="Hire Date"
+            name="hiredate"
+          />
+        </div>
+        <button className="h-8 w-16 bg-red-600 rounded-md text-lg font-medium">
+          Add
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default Employee;
